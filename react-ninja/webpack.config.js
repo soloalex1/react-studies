4e1c64b4f351cbefb8f6a248dfbf0cc31e174321
Alexandre Machado <machado@statix.com.br>
2020-09-04 15:55:25 -0300

    WIP: Webpack

---
+++ react-ninja/webpack.config.js
@@ -0,0 +1,12 @@
+'use strict'
+
+import path from 'path'
+
+module.exports = {
+    entry: path.join(__dirname, 'src', 'index'),
+    output: {
+        path: path.join(__dirname, 'dist'),
+        filename: 'bundle.js',
+        publicPath: '/static/'
+    }
+}
